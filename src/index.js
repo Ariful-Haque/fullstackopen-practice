f2e2ea99c43b2dae17d5300ffab2fc60ffa7253e
maruf <maruf@marufs-Air.zyxel.com>
2020-09-04 17:19:52 +0300

    Stateful component

--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 
 const App = (props) => {
-  const {counter} = props
-  return (
-    <div>{counter}</div>
+ const [counter, setCounter] = useState(0) 
+
+ // The function returns an array that contains two items.
+ /*
+ The counter variable is assigned the initial value of state which is zero. 
+ The variable setCounter is assigned to a function that will be used to modify the state.
+ */
+ setTimeout(
+  () => setCounter(counter + 1),
+  1000
+)
+
+console.log('rendering...', counter)
+return (
+  <div>{counter}</div>
+)
+// When the state modifying function setCounter is called, React re-renders the component
+// Every time the setCounter modifies the state it causes the component to re-render. 
+}
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
   )
-}
 
-let counter = 1
-
-
-const refresh = () => {
-  ReactDOM.render(
-    <App counter={counter} />,
-    document.getElementById('root')
-    )
-}
-
-setInterval(() => {
-  refresh()
-  counter += 1
-}, 1000)
   
