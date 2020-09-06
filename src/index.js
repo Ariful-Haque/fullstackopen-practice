98f2d0df3243ffd07d9bf6106ca0e38607f378c3
maruf <maruf@marufs-Air.zyxel.com>
2020-09-06 08:29:01 +0300

    Event handling

--- src/index.js
+++ src/index.js
@@ -10,17 +10,29 @@ const App = (props) => {
  The counter variable is assigned the initial value of state which is zero. 
  The variable setCounter is assigned to a function that will be used to modify the state.
  */
- setTimeout(
-  () => setCounter(counter + 1),
-  1000
-)
+
+// When the state modifying function setCounter is called, React re-renders the component
+// Every time the setCounter modifies the state it causes the component to re-render.
 
 console.log('rendering...', counter)
+
+
+
+const handleClick = () => {
+  console.log('clicked')
+  
+  setCounter(counter + 1)
+  
+}
+
+
 return (
+  <div>
   <div>{counter}</div>
-)
-// When the state modifying function setCounter is called, React re-renders the component
-// Every time the setCounter modifies the state it causes the component to re-render. 
+  <button onClick={handleClick} >plus</button>
+  <button onClick={() => setCounter(0)} >reset</button>
+  </div>
+) 
 }
 
 
