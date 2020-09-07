bf6f245afea0eacb9c1addaafc8cdb52cb27f636
maruf <maruf@marufs-Air.zyxel.com>
2020-09-07 20:30:52 +0300

    Passing state to child components

--- src/index.js
+++ src/index.js
@@ -1,36 +1,42 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+// --------------
+// Child componets 
+// ----------------
 
+// Display component 
+const Display = (props) => {
+  return (
+  <div>{props.counter}</div>
+  )
+}
+
+// Button componet
+const Button = (props) => {
+  return (
+  <button onClick={props.handleClick} >{props.text}</button>
+  )
+}
+
+/* ----------------
+// main component
+------------------ */
 const App = (props) => {
  const [counter, setCounter] = useState(0) 
 
- // The function returns an array that contains two items.
- /*
- The counter variable is assigned the initial value of state which is zero. 
- The variable setCounter is assigned to a function that will be used to modify the state.
- */
-
-// When the state modifying function setCounter is called, React re-renders the component
-// Every time the setCounter modifies the state it causes the component to re-render.
-
 console.log('rendering...', counter)
 
-
-
-const handleClick = () => {
-  console.log('clicked')
-  
-  setCounter(counter + 1)
-  
-}
-
+const increaseByOne = () => setCounter(counter + 1)
+const decreaseByOne = () => setCounter(counter - 1)
+const setToZero = () => setCounter(0)
 
 return (
   <div>
-  <div>{counter}</div>
-  <button onClick={handleClick} >plus</button>
-  <button onClick={() => setCounter(0)} >reset</button>
+  <Display counter={counter} />
+  <Button handleClick={increaseByOne} text='plus'/>
+  <Button handleClick={decreaseByOne} text='minus' />
+  <Button handleClick={setToZero} text='reset' />
   </div>
 ) 
 }
