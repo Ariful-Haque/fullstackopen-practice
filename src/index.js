a06b23bf52154a1ed87024f4cdf539c74531df6f
maruf <maruf@marufs-Air.zyxel.com>
2020-09-07 22:47:28 +0300

    Complex state

--- src/index.js
+++ src/index.js
@@ -1,44 +1,32 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-// --------------
-// Child componets 
-// ----------------
 
-// Display component 
-const Display = (props) => {
-  return (
-  <div>{props.counter}</div>
-  )
-}
-
-// Button componet
-const Button = (props) => {
-  return (
-  <button onClick={props.handleClick} >{props.text}</button>
-  )
-}
-
-/* ----------------
-// main component
------------------- */
 const App = (props) => {
- const [counter, setCounter] = useState(0) 
+  const [clicks, setClicks] = useState({left: 0, right: 0})
 
-console.log('rendering...', counter)
+  const handleLeftClick = () => {
+   setClicks({...clicks, left: clicks.left + 1})
+  }
 
-const increaseByOne = () => setCounter(counter + 1)
-const decreaseByOne = () => setCounter(counter - 1)
-const setToZero = () => setCounter(0)
-
-return (
-  <div>
-  <Display counter={counter} />
-  <Button handleClick={increaseByOne} text='plus'/>
-  <Button handleClick={decreaseByOne} text='minus' />
-  <Button handleClick={setToZero} text='reset' />
-  </div>
-) 
+  const handleRightClick = () => {
+    setClicks({...clicks, right: clicks.right + 1})
+  }
+  
+  return (
+    <div>
+      <div>
+        {clicks.left}
+        <button onClick={handleLeftClick}>
+          left
+        </button>
+        <button onClick={handleRightClick}>
+          right
+        </button>
+        {clicks.right}
+      </div>
+    </div>
+  )
 }
 
 
